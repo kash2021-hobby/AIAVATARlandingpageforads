ead92d621fb9e5c5466b3c8dcfc6eb591407079b
kash2021-hobby <kashyapnandan2021@gmail.com>
2025-10-04 11:49:49 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,9 +14,9 @@ function App() {
     <div className="min-h-screen bg-white">
       <Header />
       <Hero />
-      <TrustedBrands />
       <Problem />
       <Features />
+      <TrustedBrands />
       <UseCases />
       <Demo />
       <TrustedWorldwide />
